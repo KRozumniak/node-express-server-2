0fa79fb2e03ec1baaccd4365cd37fe66d25d823c
KRozumniak <kirkterra@gmail.com>
2021-01-10 01:22:56 -0500

    babel added

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-const express = require('express');
+import express from 'express';
+
 const app = express();
 const PORT = 5000;
-
 
 //localhost:5000
 
 app.get('/', home);
 app.get('/info', info);
 
-function home (req, res) {
-  res.send('PASV')
+function home(req, res) {
+  res.send('PASV---');
 }
 
-function info (req, res) {
+function info(req, res) {
   const a = 123;
   const b = Math.random();
   const c = a + b;
-  res.send('INFO here' + c)
+  res.send('INFO here' + c);
 }
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
-})
+});
