d081ad2c8b41e39e70a23f02e3a8b6692c25331b
KRozumniak <kirkterra@gmail.com>
2021-01-11 17:11:24 -0500

    cors.js updated

--- src/index.js
+++ src/index.js
@@ -3,15 +3,20 @@ import home from './home';
 import info from './info';
 import errorHandler from './errorHandler';
 import logger from './logger';
+import parseResponse from './parseResponse';
+import cors from './cors';
 
 const app = express();
 const PORT = 5000;
 
 //localhost:5000
 
+logger(app);
+parseResponse(app);
+cors(app);
+
 app.get('/', home); // GET localhost:5000/
 app.post('/info', info); // POST localhost:5000/info
-logger(app);
 
 errorHandler(app);
 
