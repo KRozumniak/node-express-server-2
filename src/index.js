439302ec8ebc4a7f61b51e9419d06f4fbeadc47a
KRozumniak <kirkterra@gmail.com>
2021-01-12 20:50:12 -0500

    DB updated

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import logger from './modules/core/logger';
 import parseResponse from './modules/core/parseResponse';
 import cors from './modules/core/cors';
 import routes from './modules/core/routes';
+import dbConnect from './modules/core/db';
 
 const app = express();
 const PORT = 5000;
 
 //localhost:5000
 
+dbConnect();
 logger(app);
 parseResponse(app);
 cors(app);
