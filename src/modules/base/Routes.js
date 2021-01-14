d356052bdd330e161e4e77ff2c6dcc86ec19b79b
KRozumniak <kirkterra@gmail.com>
2021-01-13 23:24:09 -0500

    Entity updated

--- src/modules/base/Routes.js
+++ src/modules/base/Routes.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import create from './create';
+import getAll from './getAll';
+import baseGetById from './getById';
 
 const router = Router();
 
-router.post('/', create); // POST localhost:5000/create
+router.post('/', create); // POST localhost:5000/base
+router.get('/', getAll); // GET localhost:5000/base
+router.get('/:baseId', baseGetById); // GET localhost:5000/base/:
 
 export default router;
