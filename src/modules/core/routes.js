909ae34d287db3a758bfcf20356bc57f8f506532
KRozumniak <kirkterra@gmail.com>
2021-01-25 21:58:52 -0500

    Upd

--- src/modules/core/routes.js
+++ src/modules/core/routes.js
@@ -2,7 +2,7 @@ import infoRoutes from '../info/Routes';
 import userRouter from '../user/Routes';
 import baseRouter from '../base/Routes';
 import bookRouter from '../book/Routes';
-import authorRouter from '../book/Routes';
+import authorRouter from '../author/Routes';
 
 export default function routes(app) {
   app.use('/userRegister', infoRoutes);
